f175d60f008db2e94911e3430bcca2c1643380f6
Anne Marie Johnston <28906095+coffeeipsum@users.noreply.github.com>
2018-03-16 09:31:18 +0000

    new logo and contact form

---
+++ js/main.js
@@ -0,0 +1,16 @@
+ document.getElementById('dropdownBtn').onclick = function(e) {
+            console.log('dropdown click');
+            document.getElementById("myDropdown").classList.toggle("show");
+        };
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
